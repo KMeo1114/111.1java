4fc9ff2d5d3a7c8faf5c52c3f39e076539b5b0f4
王家凱 <89355667+KMeo1114@users.noreply.github.com>
2022-09-23 11:44:05 +0800

    new

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args){
+         System.out.println(args[0]);
          System.out.println(args[1]);
          System.out.println("good job");
 
