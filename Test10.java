74d7287026e8605e82cff69e17c8b74b8f814989
王家凱 <89355667+KMeo1114@users.noreply.github.com>
2022-10-14 08:29:41 +0800

    give teath
    while

---
+++ Test10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Test10 {
+    public static void main(String[] args) {
+		int num1 = 0;
+		String str1 = "";
+		
+		while (num1 <5) {   
+		    str1 += "*";		
+			System.out.printf("%s %n",str1);
+			
+			num1 +=1;					
+		}
+		
+    }
+}
